278bb01eb0535822cd3d60642ceb6fba76d326e5
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-03-22 23:47:59 +0100

    change example training command

--- configs/train_whisper_supervised.sh
+++ configs/train_whisper_supervised.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=1 python \
+CUDA_VISIBLE_DEVICES=6 python \
     audiocap/train_whisper_supervised.py \
     --architecture-name "openai/whisper-small" \
     --checkpoint-dir-root "./checkpoints" \
-    --use-pretrained-whisper-encoder \
-    --use-pretrained-whisper-decoder \
+    --no-use-pretrained-whisper-encoder \
+    --no-use-pretrained-whisper-decoder \
     --clotho-dir "./data/clotho_v2.1" \
     --log-preds-every-n-steps 500 \
     --limit-val-split-size 250 \
