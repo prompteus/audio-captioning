e4c5138d9d3147d015470cf3fc93813460e2af6b
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-04-18 21:56:31 +0200

    update training-launching script

--- scripts/train_whisper_supervised.sh
+++ scripts/train_whisper_supervised.sh
@@ -1,10 +1,8 @@
-CUDA_VISIBLE_DEVICES=6 python \
+CUDA_VISIBLE_DEVICES=7 python \
     audiocap/train_whisper_supervised.py \
-    --architecture-name "openai/whisper-small" \
-    --checkpoint-dir-root "./checkpoints" \
-    --no-use-pretrained-whisper-encoder \
-    --no-use-pretrained-whisper-decoder \
-    --clotho-dir "./data/clotho_v2.1" \
-    --log-preds-every-n-steps 500 \
-    --limit-val-split-size 250 \
-    --training-args-config "./configs/train_whisper_supervised_train_args.yaml"
+    --checkpoint-dir-root="./checkpoints" \
+    --clotho-dir="./data/clotho_v2.1" \
+    --audioset-dir="./data/audioset_small" \
+    --audiocaps-dir="./data/audiocaps" \
+    --training-config="./configs/train_whisper_supervised_train_config.yaml" \
+    --training-phase="pretraining"
