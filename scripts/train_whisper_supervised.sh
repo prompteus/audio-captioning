d98105c1e1a50fe5a2c62f37fbe3e6bbede31106
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-04-26 00:08:29 +0200

    fix paths in trianing shell script

--- scripts/train_whisper_supervised.sh
+++ scripts/train_whisper_supervised.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=7 python \
     audiocap/train_whisper_supervised.py \
     --checkpoint-dir-root="./checkpoints" \
-    --clotho-dir="./data/clotho_v2.1" \
-    --audioset-dir="./data/audioset_small" \
-    --audiocaps-dir="./data/audiocaps" \
+    --clotho-dir="./data/clotho_v2.1/audiofolder" \
+    --audioset-dir="./data/audioset_small/audiofolder" \
+    --audiocaps-dir="./data/audiocaps/audiofolder" \
     --training-config="./configs/train_whisper_supervised_train_config.yaml"
