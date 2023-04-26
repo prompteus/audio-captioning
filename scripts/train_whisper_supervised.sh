df2dd55378ac5acdd1215b766a8a52bcf5795fbc
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-04-26 13:20:55 +0200

    add pretraining yaml config

--- scripts/train_whisper_supervised.sh
+++ scripts/train_whisper_supervised.sh
@@ -1,7 +1,8 @@
-CUDA_VISIBLE_DEVICES=7 python \
+CUDA_VISIBLE_DEVICES=3 python \
     audiocap/train_whisper_supervised.py \
     --checkpoint-dir-root="./checkpoints" \
     --clotho-dir="./data/clotho_v2.1/audiofolder" \
     --audioset-dir="./data/audioset_small/audiofolder" \
     --audiocaps-dir="./data/audiocaps/audiofolder" \
-    --training-config="./configs/train_whisper_supervised_train_config.yaml"
+    --training-config="./configs/pretrain_1on1_config_large.yaml" \
+    --wandb-group="pretraining"
