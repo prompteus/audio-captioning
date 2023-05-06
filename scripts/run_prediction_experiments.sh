9f99e52de0a1d5f65e2ecb8d72113c31513ce7d4
Adam Hajek <xhajek9@alfa.fi.muni.cz>
2023-05-06 15:57:23 +0200

    do first run of generation

--- scripts/run_prediction_experiments.sh
+++ scripts/run_prediction_experiments.sh
@@ -1,5 +1,5 @@
 
-ADDITIONAL_INFO="_!!!TEST!!!"
+ADDITIONAL_INFO="" # "_!!!TEST_first10!!!"
 
 for CONFIG_FILE_NAME in predict_clotho_beam_diverse_5_5_config \
                         predict_clotho_beam_multinomial5_config \
@@ -7,17 +7,21 @@ for CONFIG_FILE_NAME in predict_clotho_beam_diverse_5_5_config \
                         predict_clotho_greedy_config \
                         predict_clotho_sample_topk_config
 do
+    echo "Running inference with config file: ${CONFIG_FILE_NAME}.yaml"
     RUN_NAME=dainty-yogurt-57
     SPLIT=validation
     OUT_FILE_NAME=${RUN_NAME}_${SPLIT}_${CONFIG_FILE_NAME}${ADDITIONAL_INFO}
 
-    # CUDA_VISIBLE_DEVICES="..." 
-    python \
-    ../audiocap/predict.py \
-    --checkpoint ../checkpoints/$RUN_NAME/checkpoint-2600 \
-    --data ../data/clotho_v2.1/$SPLIT \
-    --output-file ../inference_outputs/${OUT_FILE_NAME}.csv \
-    --config-file ../configs/${CONFIG_FILE_NAME}.yaml \
-    --log-file ../inference_outputs/${OUT_FILE_NAME}_log.csv \
-    --take-first-n 10 # optional, for debugging purposes
+    CUDA_VISIBLE_DEVICES=0 python \
+        predict.py \
+        --checkpoint ../../maratmp/audio-captioning/checkpoints/$RUN_NAME/checkpoint-2600 \
+        --data  ../../maratmp/audio-captioning/data//clotho_v2.1/audiofolder/$SPLIT \
+        --output-file ../inference_outputs/${OUT_FILE_NAME}.csv \
+        --config-file ../configs/${CONFIG_FILE_NAME}.yaml
+        # --take-first-n 10 # optional, for debugging purposes
 done
+
+
+# alfrid paths
+# --checkpoint ../checkpoints/$RUN_NAME/checkpoint-2600 \
+# --data ../data/clotho_v2.1/$SPLIT \
